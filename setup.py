ea65c10b20aa0eac71ea5e4379c3cd02ef84b8b1
marcusmunch <marcus@marcusmunch.dk>
2017-12-27 12:58:09 +0100

    Added license and prepared setup for adding to pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
+# -*- coding:utf-8 -*-
+
 from setuptools import setup
 
 setup(name="ranktier",
 	version="1.0",
-	description="turns ranktier numbers into human-readable output",
+	description="A Dota rank tier converter",
+	long_description="Ranktier turns two-digit rank tier numbers into human-readable output.",
 	url="https://github.com/marcusmunch/ranktier",
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+		"Natural Language :: English",
+		"Programming Language :: Python :: 2.7"],
 	author="Marcus Grünewald",
 	packages=["ranktier"])
