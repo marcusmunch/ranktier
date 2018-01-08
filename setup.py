97a7a6c305170be9d1b30f0f636fa90f831098fe
marcusmunch <marcus@marcusmunch.dk>
2018-01-08 21:37:09 +0100

    Added classifiers to setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,24 @@
 from setuptools import setup
 
 setup(name="ranktier",
-	version="1.0",
+	version="1.1",
 	description="A Dota rank tier converter",
 	long_description="Ranktier turns two-digit rank tier numbers into human-readable output.",
 	url="https://github.com/marcusmunch/ranktier",
 	classifiers=[
 		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
 		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
 		"Natural Language :: English",
-		"Programming Language :: Python :: 2.7"],
+		"Operating System :: OS Independent",
+		"Programming Language :: Python :: 2",
+		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.3",
+		"Programming Language :: Python :: 3.4",
+		"Programming Language :: Python :: 3.5",
+		"Programming Language :: Python :: 3.6",
+		],
 	author="Marcus Grünewald",
 	author_email="marcus@marcusmunch.dk",
 	packages=["ranktier"])
