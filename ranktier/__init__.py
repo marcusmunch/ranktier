1eea73b8321318114c22ad729dc520edb59281ea
MarcusMunch <Marcus@MarcusMunch.dk>
2018-07-06 23:43:02 +0200

    PEP-8 style changes

--- ranktier/__init__.py
+++ ranktier/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+
 class Rank(object):
 
     def __init__(self, rank):
@@ -18,10 +19,8 @@ class Rank(object):
 
         self.rank = rank
 
-
     def __str__(cls):
         return str(cls.name)
-
 
     @property
     def name(cls):
@@ -31,12 +30,12 @@ class Rank(object):
         elif cls.rank[0] == "8":
             return "Immortal"
 
-        ranks = { "1" : "Herald"
-                , "2" : "Guardian"
-                , "3" : "Crusader"
-                , "4" : "Archon"
-                , "5" : "Legend"
-                , "6" : "Ancient"
-                , "7" : "Divine" }
+        ranks = {"1": "Herald",
+                 "2": "Guardian",
+                 "3": "Crusader",
+                 "4": "Archon",
+                 "5": "Legend",
+                 "6": "Ancient",
+                 "7": "Divine"}
 
         return "{} [{}]".format(ranks[cls.rank[0]], cls.rank[1])
