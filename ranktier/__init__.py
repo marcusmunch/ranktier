d79ffad4cceb778f779fdebd6c45a9d295bd86ab
Marcus Munch <Marcus@MarcusMunch.dk>
2019-09-18 22:13:13 +0200

    PEP-8

--- ranktier/__init__.py
+++ ranktier/__init__.py
@@ -26,13 +26,13 @@ class Rank(object):
         if cls.rank is None:
             return None
 
+        ranks = {"1": "Herald",
+                 "2": "Guardian",
+                 "3": "Crusader",
+                 "4": "Archon",
+                 "5": "Legend",
+                 "6": "Ancient",
+                 "7": "Divine"}
 
-        ranks = { "1" : "Herald"
-                , "2" : "Guardian"
-                , "3" : "Crusader"
-                , "4" : "Archon"
-                , "5" : "Legend"
-                , "6" : "Ancient"
-                , "7" : "Divine" }
 
         return "{} [{}]".format(ranks[cls.rank[0]], cls.rank[1])
