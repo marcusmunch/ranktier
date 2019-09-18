aa85abbf0f68a2d36b116a91de73828dea1a9c2e
Marcus Munch <Marcus@MarcusMunch.dk>
2019-09-18 23:38:03 +0200

    Updated test to include Player

--- test/test.py
+++ test/test.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import sys
 
 import ranktier
 
@@ -6,3 +7,6 @@ for n in [42, '42', 'null', 404]:
 	r = ranktier.Rank(n)
 
 	print(r)
+
+for steamid in sys.argv[1:]:
+	p = ranktier.Player(steamid)
